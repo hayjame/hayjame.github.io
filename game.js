e7ccf26be67b1d60b13d68896f3f8f5f7ef6eab7
hayjame <hayjame@stu.staloysius.nsw.edu.au>
2016-11-13 08:18:55 +1100

    Week 5m

--- game.js
+++ game.js
@@ -27,7 +27,7 @@ document.getElementById("counter1").innerHTML = counter + " Tacos";
        perSecondPerSecondCounter++;
        document.getElementById("counter1").innerHTML = counter + " Tacos";
        document.getElementById("clickersPerSecond").innerHTML= perSecondPerSecondCounter + " clicker(s) per 5 seconds";
-       document.getElementById("clickersPerSecondCost").innerHTML= perSecondPerSecondCounter + "1 clicker costs " + perSecondPerSecondCost + " Tacos";
+       document.getElementById("clickersPerSecondCost").innerHTML= perSecondPerSecondCounter + "1 spicy sauce costs " + perSecondPerSecondCost + " Tacos";
    }
     else {
        alert("Need 100 tacos");
