ed61d6f0881bb48e65c62530ccead16f4c15ba11
hayjame <hayjame@stu.staloysius.nsw.edu.au>
2016-11-13 07:56:02 +1100

    nd ijndjsnvd

--- game.js
+++ game.js
@@ -1,12 +1,12 @@
 var counter = 0;
 var perSecondCounter = 0;
 var perSecondCost = 10;
-var perSecondPerSecondCounter = 100;
+var perSecondPerSecondCounter = 0;
 var perSecondPerSecondCost = 100;
 function gainTacos() {
 counter = counter + 1;
 document.getElementById("counter1").innerHTML = counter + " Tacos"; 
-    }
+}
     function perSecond(){
 	if(counter >= perSecondCost) {
 		perSecondCounter++;
@@ -17,25 +17,28 @@ document.getElementById("counter1").innerHTML = counter + " Tacos";
 		document.getElementById("clickerCost").innerHTML = "1 clicker costs " + perSecondCost + " Tacos";
 	}
 	else{
-	alert("Not enough tacos!");	
+	alert("Need 10 tacos!");	
 	}
     }
-    setInterval(second,1000);
+  function perSecondPerSecond() {
+   if(counter >= perSecondPerSecondCost){
+       counter -= perSecondPerSecondCost;
+       perSecondPerSecondCounter++;
+       document.getElementById("counter1").innerHTML = counter + " Tacos";
+       document.getElementById("clickersPerSecond").innerHTML= perSecondPerSecondCounter + " clickers per 5 seconds";
+   }
+    else {
+       alert("Need 100 tacos");
+   }
+}
             function second (){
                   counter += perSecondCounter;
                   document.getElementById("counter1").innerHTML = counter + " Tacos";
 }
-function perSecondPerSecond() {
-   if(counter >= perSecondPerSecondCost){
-       counter -= perSecondPerSecondCost;
-       perSecondPerSecondCounter += 1;
-       document.getElementById("counter1").innerHTML = counter + " Tacos"
-   }
-    else {
-       alert("Not enough Tacos!!")
-   }
-}
-  setInterval(tenSecond,1000);
-            function tenSecond (){
-                  perSecondCounter += perSecondPerSecondCounterCounter;
-                  document.getElementById("p/s").innerHTML= perSecondCounter + " Tacos per second";
+  setInterval(second, 1000);
+function fiveSecond(){
+                      perSecondCounter += perSecondPerSecondCounter;
+                      document.getElementById("p/s").innerHTML= perSecondCounter + " Tacos per second";
+                      document.getElementById("clickersPerSecond").innerHTML= perSecondPerSecondCounter + " clickers per 5 seconds";
+                  }
+setInterval(fiveSecond, 5000);
